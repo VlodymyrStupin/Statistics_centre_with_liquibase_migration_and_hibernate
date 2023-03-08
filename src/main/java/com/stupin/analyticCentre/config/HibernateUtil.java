74afaacd0cb73bae9a87eb0e6f048181a23f7e1d
Volodymyr Stupin <vovastupin96@gmail.com>
2023-03-08 11:15:47 +0100

    first commit

---
+++ src/main/java/com/stupin/analyticCentre/config/HibernateUtil.java
@@ -0,0 +1,49 @@
+package com.stupin.analyticCentre.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import com.stupin.analyticCentre.entity.Device;
+import com.stupin.analyticCentre.entity.Subscriber;
+import com.stupin.analyticCentre.entity.Tariff;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import static javax.persistence.Persistence.createEntityManagerFactory;
+
+public class HibernateUtils {
+    private static SessionFactory factory;
+    private static EntityManager entityManager;
+
+    private static void configureFactory() {
+        try {
+            factory = new Configuration()
+                    .addAnnotatedClass(Device.class)
+                    .addAnnotatedClass(Subscriber.class)
+                    .addAnnotatedClass(Tariff.class)
+                    .configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    private static void configureEntityManager() {
+        EntityManagerFactory factory = createEntityManagerFactory("jdbc-hiber");
+        entityManager = factory.createEntityManager();
+    }
+
+    public static SessionFactory getFactory() {
+        if (factory == null) {
+            configureFactory();
+        }
+        return factory;
+    }
+
+    public static EntityManager getEntityManager() {
+        if (entityManager == null) {
+            configureEntityManager();
+        }
+        return entityManager;
+    }
+}
